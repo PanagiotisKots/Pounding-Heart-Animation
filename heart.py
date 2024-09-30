92a5b3eee5644606c2df36b187e605745f90c1cd
panagiotis kotsorgios <161494302+PanagiotisKots@users.noreply.github.com>
2024-09-30 18:35:11 +0300

    Update heart.py

--- heart.py
+++ heart.py
@@ -74,10 +74,10 @@ def main():
 
         # Print text below the heart
         print("\n" + " " * 5 + "------------------------------")
-        print(" " * 5 + "made for: Ntina")
         print(" " * 5 + "made by: Panagiotis Kotsorgios")
-        print(" " * 5 + "language: Python")
-        print(" " * 5 + "Description: add text here")
+        print(" " * 5 + "made for: Ntina")
+        print(" " * 5 + "language: Python3")
+        print(" " * 5 + "Description: A console based pounding heart animation")
         print("\n" + " " * 5 + "------------------------------")
 
         t += 0.01  # Increase the time increment for smoother transitions
